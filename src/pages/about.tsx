7f0b6b0caec933c2ff13cb06b2861ce064524b48
harrisongerst <hjgerstenlauer@gmail.com>
2022-08-25 16:03:40 -0400

    create about widget and make compatible with themes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,8 +13,50 @@ const blurb =
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ` as const;
 
   export function About() {
-
+    const { font, palette } = useThemeContext();
+    function renderAboutWidget() {
+        return (
+          <Container
+            padding="1rem"
+            customStyles={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start'
+            }}
+          >
+            {forwardVarText(getSafeFontKey(font.google.family), 'about', 'h1', {
+              customStyles: {
+                color: palette.headingPrimaryColor,
+                margin: '0px',
+                fontSize: '4rem'
+              }
+            })}
+            {forwardVarText(getSafeFontKey(font.google.family), blurb, 'p', {
+              customStyles: {
+                color: palette.headingPrimaryColor,
+                margin: '0px',
+                fontSize: '4rem'
+              }
+            })}
+          </Container>
+        );
+      }
     return(
-        
+        <Container width="100%" padding="0rem" id="cg-home-page-wrapping-container">
+      <OneCol
+        widgetKey="home-page-widget-one"
+        height="60vh"
+        containerProps={{
+          customStyles: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            paddingLeft: '8vw'
+          }
+        }}
+        childNode={renderAboutWidget()}
+      />
+    </Container>
     )
   }
