2b83d654979435ac1482519a2692776c572a63ef
harrisongerst <hjgerstenlauer@gmail.com>
2022-08-25 15:56:44 -0400

    add about.tsx and structure

---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { MetricType } from '@nickgdev/couch-gag-common-lib';
+import { Container, _heller_base } from '@nickgdev/hellerui';
+
+import { OneCol } from '../components/widgets/OneCol.widget';
+import { useThemeContext } from '../contexts';
+import { emit } from '../service/metric';
+import { forwardVarText, getSafeFontKey } from '../utils';
+import SlideIn from '../components/animated/SlideIn';
+
+const blurb =
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ` as const;
+
+  export function About() {
+
+    return(
+        
+    )
+  }
